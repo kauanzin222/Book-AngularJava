aeac99ecb2d96a2eccd24d1530c3ce2ffdffdd29
kauanzin222 <kauaanthony.cardoso44@gmail.com>
2025-11-10 18:11:13 -0300

    adicionando bootstrap

--- src/app/app.ts
+++ src/app/app.ts
@@ -7,5 +7,5 @@ import { Component, signal } from '@angular/core';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('modulo8-book');
+  protected readonly title = signal('Livros S/A');
 }
